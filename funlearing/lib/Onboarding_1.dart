a6491b64484c06a14825336ce07b55d758eb278b
ADIBA NOOR <adiba.noor003@gmail.com>
2022-09-15 16:17:16 +0530

    added splash screen

---
+++ funlearing/lib/Onboarding_1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Onboarding_1 extends StatefulWidget {
+  const Onboarding_1({Key? key}) : super(key: key);
+  @override
+  State<Onboarding_1> createState() => _Onboarding_1State();
+}
+
+class _Onboarding_1State extends State<Onboarding_1> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
