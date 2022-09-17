d88211706e1d457cc68ea8eb513b672717d7c7b0
ADIBA NOOR <adiba.noor003@gmail.com>
2022-09-18 01:57:17 +0530

    done some directory related changes

--- funlearing/lib/pages/HomePage.dart
+++ funlearing/lib/pages/HomePage.dart
@@ -11,7 +11,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex=0;
-  
+
   final List<Widget> _pages = [
     Row(children: [
       Text('Home',
--- funlearing/test/widget_test.dart
+++ funlearing/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:funlearing/pages/main.dart';
+import 'package:funlearing/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
